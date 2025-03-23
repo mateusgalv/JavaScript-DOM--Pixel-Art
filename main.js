34476267042818c7a5a8fe0c0c3ad54cfbb566fd
Mateus Galvao <mateustgalvao@outlook.com>
2025-03-23 10:54:27 -0300

    adjusts reset btn behavior

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const BLACK = 'rgb(0, 0, 0)';
+const MIN_GRID_SIZE = 15;
+const MAX_GRID_SIZE = 40;
 
 const grid = document.getElementById('grid-container');
 const colorPicker = document.getElementById('color-selector-container');
@@ -6,8 +8,10 @@ const fixedColorsPicker = document.getElementById('fixed-colors-container');
 const randomButton = document.getElementById('random-color-button');
 const setSizeBtn = document.getElementById('set-size-button');
 const resetBtn = document.getElementById('reset-button');
+const saveBtn = document.getElementById('save-button');
 const inputColor = document.getElementById('manual-color');
 const blackCard = document.getElementById('default-black');
+const gridSize = document.getElementById('grid-size');
 
 const randomColor = () => {
     const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0');
@@ -65,7 +69,7 @@ const removeCurrentColors = () => {
     }
 };
 
-const generateGrid = (size = 15) => {
+const generateGrid = (size = MIN_GRID_SIZE) => {
     for(let i = 0; i < size; i++) {
         const rowContainer = document.createElement('div');
         rowContainer.classList.add('row-container');
@@ -103,11 +107,16 @@ randomButton.addEventListener('click', () => {
     }
 });
 
+saveBtn.addEventListener('click', () => {
+    const actualGrid = grid.children;
+
+    console.log(actualGrid);
+});
+
 setSizeBtn.addEventListener('click', () => {
-    const gridSize = document.getElementById('grid-size');
     const size = gridSize.value;
 
-    if(size > gridSize.max || size < gridSize.min) {
+    if(size > MAX_GRID_SIZE || size < MIN_GRID_SIZE) {
         console.log("Invalid grid size");
         window.alert("Invalid grid size\nSelect a value between 15 and 40");
     } else {
@@ -119,6 +128,7 @@ setSizeBtn.addEventListener('click', () => {
 resetBtn.addEventListener('click', () => {
     removeCurrentGrid();
     generateGrid();
+    gridSize.value = MIN_GRID_SIZE;
 });
 
 window.onload = () => {
